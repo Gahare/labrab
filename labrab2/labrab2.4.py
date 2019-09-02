c38be225aecd2d5e0bd5321b6821b232b87cfa1c
Gahare <prohorshubenkov12@gmail.com>
2019-09-02 21:42:41 +0300

    New code

---
+++ labrab2/labrab2.4.py
@@ -0,0 +1,9 @@
+import math
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+h = abs(y2-y1)
+w = abs(x2-x1)
+print(h*w)
+print(h*2+w*2)
---
+++ labrab2/labrab2.5.py
@@ -0,0 +1,9 @@
+import math
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+s = abs(((x1-x3)*(y2-y3)-(y1-y3)*(x2-x3))/2)
+print(s)
