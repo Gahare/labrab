4030ac8893c3218b574b632ccaf0df293fd752d9
Gahare <prohorshubenkov12@gmail.com>
2019-09-09 12:58:31 +0300

    last

--- labrab1(done)/labrab1.4.py
+++ labrab1(done)/labrab1.4.py
@@ -1,6 +1,10 @@
+#сумма разность произведение квадратов
 a = float(input('Введите первое число:'))
 b = float(input('Введите второе число:'))
-print(a * a + b * b)
-print(a * a - b * b)
-print((a * a) * (b * b))
-print(round((a * a) / (b * b), 5))
+if a==0 or b==0:
+    print('Некорректные данные')
+else:
+    print(a * a + b * b)
+    print(a * a - b * b)
+    print((a * a) * (b * b))
+    print(round((a * a) / (b * b), 5))
--- labrab1(done)/labrab1.5.py
+++ labrab1(done)/labrab1.5.py
@@ -1,6 +1,10 @@
+#сумма разность произведние модулей
 a = float(input('Введите первое число:'))
 b = float(input('Введите второе число:'))
-print(abs(a) + abs(b))
-print(abs(a) - abs(b))
-print(abs(a) * abs(b))
-print(round(abs(a) / abs(b), 5))
+if a==0 or b==0:
+    print('некорректные данные')
+else:
+    print(abs(a) + abs(b))
+    print(abs(a) - abs(b))
+    print(abs(a) * abs(b))
+    print(round(abs(a) / abs(b), 5))
