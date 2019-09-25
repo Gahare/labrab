50a1ccfc8ccb1b779bab56109018f154b4f71994
Gahare <prohorshubenkov12@gmail.com>
2019-09-25 23:05:53 +0300

    update

--- labrab19/labrab19.4.py
+++ labrab19/labrab19.4.py
@@ -1,12 +1,13 @@
 m = int(input())  # vniz
 n = int(input())  # vpravo
 a = [0] * n
-firstmas = []
-curmas = []
 for i in range(n):
     a[i] = [0] * m
 for i in range(n):
     for j in range(m):
         a[i][j] = float(input())
-for i in range(n):
-    firstmas.append(a[i][0])
+for j in range(n - 1):
+    for i in range(n - 1):
+        if a[i][0] > a[i + 1][0]:
+            a[i], a[i + 1] = a[i + 1], a[i]
+print(a)
